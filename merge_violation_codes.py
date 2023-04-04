78440cbcad96f29cf48c6777aa9fcacff25fc542
Agape Seo <agapeseo@gmail.com>
2023-04-04 00:21:44 -0500

    merge_violation code added

---
+++ merge_violation_codes.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv("dataCleaned1.csv", low_memory=False)
+
+clean_violation = pd.DataFrame(columns=['Violation Code'])
+
+for row in range(len(df['Violation Code'])):
+    code = str(df['Violation Code'][row])
+    if 'MP' in code:
+        clean_code = code[2:]
+        clean_code = clean_code.lstrip('0')
+        clean_violation.loc[row] = clean_code
+
+    elif 'P' in code:
+        clean_code = code[1:]
+        clean_code = clean_code.lstrip('0')
+        clean_violation.loc[row] = clean_code
+
+df['Violation Code'] = clean_violation['Violation Code']
+df.to_csv("violations_merged.csv", index=False)
